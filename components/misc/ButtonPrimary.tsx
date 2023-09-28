81dbd3b38e0396997d75400818775c0228ef36e4
hpaulsalazar96 <hpaulsalazar96@hotmail.com>
2023-09-28 10:05:49 -0500

    add buttons components

---
+++ components/misc/ButtonPrimary.tsx
@@ -0,0 +1,18 @@
+type Props = {
+    addClass?: string,
+    children?: React.ReactNode
+}
+
+export const ButtonPrimary = ({ children, addClass }: Props) => {
+    return (
+      <button
+        className={
+          "py-3 lg:py-4 px-12 lg:px-16 text-white-500 font-semibold rounded-lg bg-orange-500 hover:shadow-orange-md transition ease-in-out duration-300 outline-none " +
+          addClass
+        }
+      >
+        {children}
+      </button>
+    )
+  }
+  
