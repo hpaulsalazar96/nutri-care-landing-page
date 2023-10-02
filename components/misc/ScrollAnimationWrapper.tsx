a74535f6e0bf25c4cadabf11a7f466a63d2dc203
hpaulsalazar96 <hpaulsalazar96@hotmail.com>
2023-10-02 16:55:20 -0500

    scroll animation added

---
+++ components/misc/ScrollAnimationWrapper.tsx
@@ -0,0 +1,19 @@
+import {motion} from "framer-motion";
+
+type Props = { 
+    children: React.ReactNode 
+    className:string
+};
+
+export const ScrollAnimationWrapper: React.FC<Props> = ({children, className}) => {
+  return (
+    <motion.div
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+      className={className}
+    >
+      {children}
+    </motion.div>
+  )
+}
