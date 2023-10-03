5277ddadfb88b5243a6855598fdbf5cb22ad8e2c
hpaulsalazar96 <hpaulsalazar96@hotmail.com>
2023-10-03 15:32:03 -0500

    testimonial responsive fixed buttons position

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -3,7 +3,7 @@
 import Slider from "react-slick"
 import Image from "next/image"
 import { StarIcon, ArrowLeftIcon, ArrowRightIcon, UserCircleIcon } from "@heroicons/react/20/solid";
-import { useMemo, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { testimonialsList } from "./utils/testimonialsList"
 import { ScrollAnimationWrapper } from "./misc/ScrollAnimationWrapper";
 import { motion } from "framer-motion";
@@ -47,6 +47,23 @@ export const Testimonial = ({ }) => {
     }
     const [sliderRef, setSliderRef] = useState<any>(null);
 
+    const [smallScreen, setSmallScreen] = useState(false);
+
+    useEffect(() => {
+        const updateScreenSize = () => {
+            if (window.innerWidth >= 720) {
+                setSmallScreen(false)
+            } else {
+                setSmallScreen(true)
+            }
+        }
+        updateScreenSize();
+        window.addEventListener('resize', updateScreenSize);
+        return () => {
+            window.removeEventListener('resize', updateScreenSize);
+        };
+    }, []);
+
     return (
         <>
             <div
@@ -59,80 +76,154 @@ export const Testimonial = ({ }) => {
                             <ScrollAnimationWrapper className={""}>
                                 <motion.h3
                                     variants={scrollAnimation}
-                                    className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto">
+                                    className="text-2xl sm:text-3xl lg:text-4xl font-medium text-nutricare-200 leading-normal w-9/12 sm: lg:w-4/12 mx-auto">
                                     Testimonios{" "}
                                 </motion.h3>
                                 <motion.p
                                     variants={scrollAnimation}
                                     className="leading-normal mx-auto mb-2 mt-4 w-10/12 sm:w-7/12 lg:w-6/12"
                                 >
-                                    Estos son los relatos de algunos de nuestros clientes.
+                                    Estos son los relatos de algunos de nuestros clientes. <br />
+                                        Describen como fue su experiencia con Nutri Care
                                 </motion.p>
                             </ScrollAnimationWrapper>
                         </div>
                         <ScrollAnimationWrapper className="w-full flex flex-col py-12">
                             <motion.div variants={scrollAnimation}>
-                                <Slider
-                                    {...settings}
-                                    arrows={false}
-                                    ref={setSliderRef}
-                                    className="flex items-stretch justify-items-stretch"
-                                >
-                                    {testimonialsList.map((listTestimonis, index) => (
-                                        <div className="px-3 flex items-stretch" key={index}>
-                                            <div className="border-2 border-gray-500 hover:border-nutricare-100 transition-all rounded-lg p-8 flex flex-col">
-                                                <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
-                                                    <div className="flex order-2 xl:order-1">
-                                                        {
-                                                            listTestimonis.image
-                                                                ?
-                                                                <Image
-                                                                    src={listTestimonis.image}
-                                                                    height={50}
-                                                                    width={50}
-                                                                    alt="Icon People"
-                                                                />
-                                                                :
-                                                                <UserCircleIcon className="h-12 w-12 text-nutricare-100" />
-                                                        }
+                                {
+                                    !smallScreen
+                                        ?
+                                        (<>
+                                            <Slider
+                                                {...settings}
+                                                arrows={false}
+                                                ref={setSliderRef}
+                                                className="flex items-stretch justify-items-stretch"
+                                            >
+                                                {testimonialsList.map((listTestimonis, index) => (
+                                                    <div className="px-3 flex items-stretch" key={index}>
+                                                        <div className="border-2 border-gray-500 hover:border-nutricare-100 transition-all rounded-lg p-8 flex flex-col">
+                                                            <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
+                                                                <div className="flex order-2 xl:order-1">
+                                                                    {
+                                                                        listTestimonis.image
+                                                                            ?
+                                                                            <Image
+                                                                                src={listTestimonis.image}
+                                                                                height={50}
+                                                                                width={50}
+                                                                                alt="Icon People"
+                                                                            />
+                                                                            :
+                                                                            <UserCircleIcon className="h-12 w-12 text-nutricare-100" />
+                                                                    }
 
-                                                        <div className="flex flex-col ml-5 text-left">
-                                                            <p className="text-lg text-nutricare-200 capitalize">
-                                                                {listTestimonis.name}
-                                                            </p>
-                                                            <p className="text-sm text-nutricare-300 capitalize">
-                                                                {listTestimonis.career}
-                                                            </p>
+                                                                    <div className="flex flex-col ml-5 text-left">
+                                                                        <p className="text-lg text-nutricare-200 capitalize">
+                                                                            {listTestimonis.name}
+                                                                        </p>
+                                                                        <p className="text-sm text-nutricare-300 capitalize">
+                                                                            {listTestimonis.career}
+                                                                        </p>
+                                                                    </div>
+                                                                </div>
+                                                                <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
+                                                                    <p className="text-sm text-nutricare-200">{listTestimonis.rating}</p>
+                                                                    <span className="flex ml-4">
+                                                                        <StarIcon className="h-4 w-4 text-nutricare-100" />
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <p className="mt-5 text-left text-nutricare-200">“{listTestimonis.testimoni}”</p>
                                                         </div>
                                                     </div>
-                                                    <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
-                                                        <p className="text-sm text-nutricare-200">{listTestimonis.rating}</p>
-                                                        <span className="flex ml-4">
-                                                            <StarIcon className="h-4 w-4 text-nutricare-100" />
-                                                        </span>
+                                                ))}
+                                            </Slider>
+                                            <div className="flex w-full items-center justify-end">
+                                                <div className="flex flex-none justify-between w-auto mt-14">
+                                                    <div
+                                                        className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-nutricare-100 border hover:bg-nutricare-200 hover:text-white-500 hover:border-nutricare-200 transition-all text-nutricare-100 cursor-pointer"
+                                                        onClick={sliderRef?.slickPrev}
+                                                    >
+                                                        <ArrowLeftIcon className="h-6 w-6 " />
+                                                    </div>
+                                                    <div
+                                                        className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-nutricare-100 border hover:bg-nutricare-200 hover:text-white-500 hover:border-nutricare-200 transition-all text-nutricare-100 cursor-pointer"
+                                                        onClick={sliderRef?.slickNext}
+                                                    >
+                                                        <ArrowRightIcon className="h-6 w-6" />
                                                     </div>
                                                 </div>
-                                                <p className="mt-5 text-left text-nutricare-200">“{listTestimonis.testimoni}”</p>
                                             </div>
-                                        </div>
-                                    ))}
-                                </Slider>
-                                <div className="flex w-full items-center justify-end">
-                                    <div className="flex flex-none justify-between w-auto mt-14">
-                                        <div
-                                            className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-nutricare-100 border hover:bg-nutricare-200 hover:text-white-500 hover:border-nutricare-200 transition-all text-nutricare-100 cursor-pointer"
-                                            onClick={sliderRef?.slickPrev}
-                                        >
-                                            <ArrowLeftIcon className="h-6 w-6 " />
-                                        </div>
-                                        <div
-                                            className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-nutricare-100 border hover:bg-nutricare-200 hover:text-white-500 hover:border-nutricare-200 transition-all text-nutricare-100 cursor-pointer"
-                                            onClick={sliderRef?.slickNext}
-                                        >
-                                            <ArrowRightIcon className="h-6 w-6" />
-                                        </div>
-                                    </div>
-                                </div>
+                                        </>
+                                        )
+                                        :
+                                        (<>
+                                            <Slider
+                                                {...settings}
+                                                arrows={false}
+                                                ref={setSliderRef}
+                                                className="flex items-stretch justify-items-stretch"
+                                            >
+                                                {testimonialsList.map((listTestimonis, index) => (
+                                                    <div className="px-3 flex items-stretch" key={index}>
+                                                        <div className="border-2 border-gray-500 hover:border-nutricare-100 transition-all rounded-lg p-8 flex flex-col">
+                                                            <div className="flex flex-col xl:flex-row w-full items-stretch xl:items-center">
+                                                                <div className="flex order-2 xl:order-1">
+                                                                    {
+                                                                        listTestimonis.image
+                                                                            ?
+                                                                            <Image
+                                                                                src={listTestimonis.image}
+                                                                                height={50}
+                                                                                width={50}
+                                                                                alt="Icon People"
+                                                                            />
+                                                                            :
+                                                                            <UserCircleIcon className="h-12 w-12 text-nutricare-100" />
+                                                                    }
+
+                                                                    <div className="flex flex-col ml-5 text-left">
+                                                                        <p className="text-lg text-nutricare-200 capitalize">
+                                                                            {listTestimonis.name}
+                                                                        </p>
+                                                                        <p className="text-sm text-nutricare-300 capitalize">
+                                                                            {listTestimonis.career}
+                                                                        </p>
+                                                                    </div>
+                                                                </div>
+                                                                <div className="flex flex-none items-center ml-auto order-1 xl:order-2">
+                                                                    <p className="text-sm text-nutricare-200">{listTestimonis.rating}</p>
+                                                                    <span className="flex ml-4">
+                                                                        <StarIcon className="h-4 w-4 text-nutricare-100" />
+                                                                    </span>
+                                                                </div>
+                                                            </div>
+                                                            <p className="mt-5 text-left text-nutricare-200">“{listTestimonis.testimoni}”</p>
+                                                        </div>
+                                                    </div>
+                                                ))}
+                                            </Slider>
+                                            <div className="relative rigth-15 flex w-full items-center justify-center pt-2">
+                                                <div className="absolute flex justify-center w-auto mt-14">
+                                                    <div
+                                                        className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-nutricare-100 border hover:bg-nutricare-200 hover:text-white-500 hover:border-nutricare-200 transition-all text-nutricare-100 cursor-pointer"
+                                                        onClick={sliderRef?.slickPrev}
+                                                    >
+                                                        <ArrowLeftIcon className="h-6 w-6 " />
+                                                    </div>
+                                                    <div
+                                                        className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-nutricare-100 border hover:bg-nutricare-200 hover:text-white-500 hover:border-nutricare-200 transition-all text-nutricare-100 cursor-pointer"
+                                                        onClick={sliderRef?.slickNext}
+                                                    >
+                                                        <ArrowRightIcon className="h-6 w-6" />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </>
+                                        )
+                                }
+
                             </motion.div>
                         </ScrollAnimationWrapper>
 
--- components/utils/testimonialsList.js
+++ components/utils/testimonialsList.js
@@ -49,11 +49,27 @@ export const testimonialsList = [
     },
     {
         name: "Carlos Rodríguez",
-        image: "/testimonials/cr.jpg",
+        image: "/testimonials/jp.jpg",
         career: "Ingeniero Civil",
         rating: "5.0",
         testimoni:
             "NutriCare me enseñó a equilibrar mi dieta mientras trabajaba largas horas. Mi nutricionista en línea diseñó un plan que me ayudó a perder peso y mejorar mi salud en general.",
+    },
+    {
+        name: "Laura Martínez",
+        image: "/testimonials/lm.jpg",
+        career: "Estudiante universitaria",
+        rating: "5.0",
+        testimoni:
+            "El estres de la universidad afectó  a mi dieta, NutriCare me dio un enfoque realista y asequible para la nutrición. Perdí peso, me siento más concentrada y confiada.",
+    },
+    {
+        name: "Laura Martínez",
+        image: "/testimonials/lm.jpg",
+        career: "Estudiante universitaria",
+        rating: "5.0",
+        testimoni:
+            "El estres de la universidad afectó  a mi dieta, NutriCare me dio un enfoque realista y asequible para la nutrición. Perdí peso, me siento más concentrada y confiada.",
     },
     {
         name: "Laura Martínez",
