0722e98fbf28e01593d0705e2e535d719151f8d1
hpaulsalazar96 <hpaulsalazar96@hotmail.com>
2023-10-04 18:02:35 -0500

    add calendly widget

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    env: {
+        NEXT_PUBLIC_CALENDLY_USERNAME: 'nutricareweb',
+        NEXT_PUBLIC_CALENDLY_SHOW_DETAILS: 'true',
+        NEXT_PUBLIC_CALENDLY_SHOW_COOKIES: 'true',
+    },
+}
 
 module.exports = nextConfig
