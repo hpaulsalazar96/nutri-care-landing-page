71069150d89b18c209f92e44f8062278c4c3e413
hpaulsalazar96 <hpaulsalazar96@hotmail.com>
2023-10-04 10:02:04 -0500

    hide scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="overflow-y-scroll no-scrollbar">{children}</body>
     </html>
   )
 }
